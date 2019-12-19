de64b5232d9240fae3a4543c75325556c549ec3a
root <root@bastion.254f.example.opentlc.com>
2019-12-19 18:20:25 +0000

    .

---
+++ env_var.sh
@@ -0,0 +1,8 @@
+export TOWER_GUID=254f
+export OSP_GUID=0614
+export OPENTLC_LOGIN=andre.cucatti-ibm.com
+export OPENTLC_PASSWORD=Z9dd4$VK@ijTdfm
+export GITHUB_REPO=https://github.com/acucatti/ansible_advance_homework
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=us-east-1
+export RH_MAIL_ID=andre.cucatti@ibm.com
